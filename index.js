cb766a85dac4b8825749ee72ef8e75985d717e3c
Bastien Michaux <bastienmichaux@gmail.com>
2017-07-12 12:19:32 +0200

    begin refactoring

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-const chalk = require('chalk');
-const fs = require('fs');
-const util = require('util');
-
-const connect = require('./lib/connect.js');
-const importer = require('./lib/importer.js');
+// dbi library
 const cst = require('./lib/constants.js');
 const prompt = require('./lib/prompt.js');
 
-/**
- * If the module is used as it is,
- * this function is called to greet the user.
- */
+// database drivers
+const dbiMysql = require('./lib/mysql.js');
+
+
+// database driver selection
+const getDatabaseDriver = (databaseType) => {
+    if (databaseType === 'mysql') {
+        return dbiMysql;
+    }
+    else {
+        throw new Error(`No driver for db '${databaseType}'`);
+    }
+};
+
+
+const testConnection = (connectionObject) => {
+    connectionObject.query('SELECT 1+1 AS solution', function (error, results, fields) {
+        if (error) {
+            throw error;
+        }
+        return results;
+    });
+};
+
 const start = () => {
-    const getFilepath = tableName => `dump/${tableName}.json`;
-
-    // oracle, mysql, sqlite, etc
-    let dbType = null;
-
-    // user, password, host address, database name
-    let credentials = null;
-
-     // knex connection object // eslint is derping here
     let connection = null;
+    let databaseDriver = null;
+    let selectedDatabase = null;
 
-    // the database tables as an array of strings
-    let tables = null;
-
-    // store the errors of rejected promises
+    // stores the promise rejection errors
     const errors = {};
 
-    // what we will write to the files
-    const fileContent = null;
-
-    const filePath = null;
-
+    // greet the user
     console.log(`${cst.messages.cat} ${cst.messages.hello}`);
 
+    // ask the database type
     prompt.askDatabaseType()
-    // depending on the database type, ask the appropriate credentials
+    // knowing the database type, ask the appropriate credentials
     .then(
         (onFulfilled) => {
-            dbType = onFulfilled;
-            return prompt.askCredentials(dbType);
+            selectedDatabase = onFulfilled;
+            databaseDriver = getDatabaseDriver(selectedDatabase);
+            return prompt.askCredentials(selectedDatabase);
         },
         (onRejected) => {
             errors.askDatabaseType = onRejected;
@@ -49,43 +52,17 @@ const start = () => {
             console.log(errors);
         }
     )
-    // given the connection credentials, attempt to connect to the db
-    // TODO: catch connection errors
-    // (they're not caught at the moment, don't know why)
+    // having the credentials, connect to the database
     .then(
         (onFulfilled) => {
             credentials = onFulfilled;
-            credentials.databaseType = dbType;
-            connection = connect.getConnectionObject(credentials);
+            connection = databaseDriver.getConnectionObject(credentials);
+            console.log(connection);
+            queryResult = x(connection);
         },
         (onRejected) => {
             errors.askCredentials = onRejected;
             console.log('Promise rejected on prompt/askCredentials');
-            console.log(errors);
-        }
-    )
-    // having a connection to the db, fetch all the database tables
-    .then(
-        (onFulfilled) => {
-            console.log(chalk.bold(`Node-db-importer: Fetching all the tables for the database '${credentials.database}'`));
-            return importer.getTableNames(credentials);
-        },
-        (onRejected) => {
-            errors.getMysqlConnectionObject = onRejected;
-            console.log(chalk.bold('Promise rejected on connect/getConnectionObject'));
-            console.log(errors);
-        }
-    )
-    .then(
-        (onFulfilled) => {
-            // here, all database tables will be written in the ./dump folder
-            tables = onFulfilled;
-            console.log(chalk.bold(`Node-db-importer: Found ${tables.length} tables`));
-            importer.getAllTables(credentials, tables);
-        },
-        (onRejected) => {
-            errors.getMysqlTableNames = onRejected;
-            console.log(chalk.bold('Promise rejected on importer/getTableNames'));
             console.log(errors);
         }
     );
