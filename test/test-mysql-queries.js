01c1cd41c72bcbd4d6abab11104cbea3b7f88c67
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-07-17 10:40:00 +0200

    add mysql expectedManyToManyQuery

--- test/test-mysql-queries.js
+++ test/test-mysql-queries.js
@@ -72,6 +72,33 @@ WHERE tab.TABLE_SCHEMA = 'elearning'
       AND ke.REFERENCED_TABLE_NAME IS NULL
 ;`;
 
+const expectedManyToManyQuery = `
+SELECT
+    ke.TABLE_NAME AS 'JUNCTION_TABLE',
+    GROUP_CONCAT(ke.COLUMN_NAME ORDER BY ke.REFERENCED_TABLE_NAME ASC) AS 'FOREIGN_KEYS',
+    GROUP_CONCAT(ke.REFERENCED_TABLE_NAME ORDER BY ke.REFERENCED_TABLE_NAME ASC) AS 'REFERENCED_TABLES'
+FROM KEY_COLUMN_USAGE ke
+
+    LEFT JOIN COLUMNS col
+        ON col.TABLE_NAME = ke.TABLE_NAME
+           AND col.COLUMN_NAME = ke.COLUMN_NAME
+           AND col.TABLE_SCHEMA = ke.TABLE_SCHEMA
+
+WHERE ke.REFERENCED_TABLE_SCHEMA = 'elearning'
+      -- exclude JHipster Tables
+      AND ke.TABLE_NAME NOT LIKE 'jhi\\_%'
+      -- exclude liquibase tables
+      AND ke.TABLE_NAME NOT IN ('DATABASECHANGELOG', 'DATABASECHANGELOGLOCK')
+      -- only relationship constraints, this excludes indexes and alike
+      AND ke.REFERENCED_TABLE_NAME IS NOT NULL
+      -- only many-to-many relationships
+      AND col.COLUMN_KEY = 'PRI'
+
+-- ensure we only get junction tables between two tables.
+GROUP BY ke.TABLE_NAME
+HAVING COUNT(ke.TABLE_NAME) = 2
+;`;
+
 describe('./lib/mysql/queries.js', () => {
     const connection = {
         escape: sqlstring.escape
