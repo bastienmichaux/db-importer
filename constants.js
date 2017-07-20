4a6efc21f283cfc5bdb0c5fe5f0068e8fdfef7e6
AdrienHorgnies <adrien.pierre.horgnies@gmail.com>
2017-07-20 12:31:10 +0200

    directly prompt user from index.js

--- constants.js
+++ constants.js
@@ -1,21 +1,32 @@
 /** Constants */
 
 const chalk = require('chalk');
+const toArray = require('lodash').values;
 
 const packageInfo = require('./package.json');
 
 
-const DBMSs = {
+const dbmsList = {
     mysql: 'mysql'
 };
 
-const messages = {
-    cat: `${chalk.bgCyan.black('/ᐠ｡ꞈ｡ᐟ\\')}`,
-    hello: `${chalk.bold(`Oh hai. I'm Node-db-importer v${packageInfo.version}.\nI need information before importing your db.`)}`
+const inquiries = {
+    dbms: {
+        type: 'list',
+        name: 'dbms',
+        message: 'Database type?',
+        choices: toArray(dbmsList),
+        default: dbmsList.mysql
+    }
 };
 
+const messages = {
+    greeting: `${chalk.bgCyan.black('/ᐠ｡ꞈ｡ᐟ\\')} ${chalk.bold(`Oh hai. I'm Node-db-importer v${packageInfo.version}.
+I need information before importing your db.`)}`
+};
 
 module.exports = {
-    databaseTypes: DBMSs,
+    dbmsList,
+    inquiries,
     messages
 };
--- index.js
+++ index.js
@@ -1,67 +1,17 @@
 // dbi library
-const cst = require('./constants.js');
-const prompt = require('./lib/prompt.js');
+const ask = require('inquirer').prompt;
+const cst = require('./constants');
 
-// database drivers
-const dbiMysql = require('./lib/mysql.js');
+const inquiries = cst.inquiries;
+const msg = cst.messages;
 
-
-// database driver selection
-const getDatabaseDriver = (databaseType) => {
-    if (databaseType === 'mysql') {
-        return dbiMysql;
-    }
-    throw new Error(`No driver for db '${databaseType}'`);
-};
 
 const start = () => {
-    let credentials = {};
-    let databaseDriver = null;
-    let selectedDatabase = null;
-    let tables = [];
+    console.log(msg.greeting);
 
-    // stores the promise rejection errors
-    const errors = {};
-
-    // greet the user
-    console.log(`${cst.messages.cat} ${cst.messages.hello}`);
-
-    // ask the database type
-    prompt.askDatabaseType()
-    // knowing the database type, ask the appropriate credentials
-        .then(
-            (onFulfilled) => {
-                selectedDatabase = onFulfilled;
-                databaseDriver = getDatabaseDriver(selectedDatabase);
-                return prompt.askCredentials(selectedDatabase);
-            },
-            (onRejected) => {
-                errors.askDatabaseType = onRejected;
-                console.log('Promise rejected on prompt/askDatabaseType');
-                console.log(errors);
-            }
-        )
-    // having the credentials, connect to the database and get all the tables
-        .then(
-            (onFulfilled) => {
-                credentials = onFulfilled;
-                credentials.databaseType = selectedDatabase;
-                return databaseDriver.getTables(credentials);
-            },
-            (onRejected) => {
-                errors.askCredentials = onRejected;
-                console.log('Promise rejected on prompt/askCredentials');
-                console.log(errors);
-            }
-        )
-    // having all the database tables,
-    // get their description (fields, constraints, keys, etc)
-        .then(
-            (onFulfilled) => {
-                tables = onFulfilled;
-                console.log(tables);
-            }
-        );
+    ask(inquiries.dbms).then((answer) => {
+        console.log(`You chose ${answer.dbms}`);
+    });
 };
 
 start();
