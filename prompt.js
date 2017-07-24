42d6e924dec3cc1bd9d91de751a279ecb0495677
AdrienHorgnies <aHorgnies@altissia.com>
2017-07-24 14:32:11 +0200

    test inquirer.prompt args for askCredentials inner call

--- prompt.js
+++ prompt.js
@@ -6,15 +6,15 @@ const inquiries = cst.inquiries;
 const askCredentials = () => {
     const credentials = {};
 
-    return inquirer.prompt(inquiries.dbms);
-        // .then((onFulfilled) => {
-        //     credentials.dbms = onFulfilled.dbms;
-        //
-        //     inquiries.port.default = cst.dbmsList[credentials.dbms].defaultPort;
-        //
-        //     return inquirer.prompt([inquiries.host, inquiries.port, inquiries.user, inquiries.password]);
-        // })
-        // .then(onFulfilled => Object.assign(credentials, onFulfilled));
+    return inquirer.prompt(inquiries.dbms)
+        .then((onFulfilled) => {
+            credentials.dbms = onFulfilled.dbms;
+
+            inquiries.port.default = cst.dbmsList[credentials.dbms].defaultPort;
+
+            return inquirer.prompt([inquiries.host, inquiries.port, inquiries.user, inquiries.password]);
+        })
+        .then(onFulfilled => Object.assign(credentials, onFulfilled));
 };
 
 module.exports = {
--- test/test-prompt.js
+++ test/test-prompt.js
@@ -1,13 +1,31 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const inquirer = require('inquirer');
+
 const prompt = require('../prompt');
 const cst = require('../constants');
 
-describe('askCredentials', function () {
-    it('returns a fulfilled promise containing complete credentials', function () {
-        const askStub = sinon.stub(inquirer, 'prompt').withArgs(cst.inquiries.dbms).resolves('mysql');
+const inq = cst.inquiries;
 
-        console.log(prompt.askCredentials());
-            // .then((onFulfilled) => {
-            //     console.log(askStub.callCount);
-            // });
+describe('askCredentials', function () {
+    it('provides the correct default for the chosen dbms', function () {
+        const askStub = sinon.stub(inquirer, 'prompt').onCall(0).resolves({ dbms: cst.dbmsList.mysql.name });
+        askStub.onCall(1).resolves(null);
+
+        const expectedPortInq = Object.assign({ default: 3306 }, inq.port);
+
+        return prompt.askCredentials()
+            .then((onFulfilled) => {
+                assert.deepEqual(askStub.getCall(1).args[0], [inq.host, expectedPortInq, inq.user, inq.password]);
+            });
+    });
+
+    it.skip('returns a fulfilled promise containing complete credentials', function () {
+        const askStub = sinon.stub(inquirer, 'prompt').withArgs(inq.dbms).resolves('mysql');
+
+        console.log(prompt.askCredentials())
+            .then((onFulfilled) => {
+                console.log(askStub.callCount);
+            });
     });
 });
