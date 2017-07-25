abdbe37867e14217ffdac9fd3d7d152362d43fc6
AdrienHorgnies <aHorgnies@altissia.com>
2017-07-25 11:15:57 +0200

    sandbox sinon stub

--- test/test-prompt.js
+++ test/test-prompt.js
@@ -5,23 +5,28 @@ const inquirer = require('inquirer');
 const prompt = require('../prompt');
 const cst = require('../constants');
 
+const sandbox = sinon.sandbox.create();
 const inq = cst.inquiries;
 
 describe('askCredentials', function () {
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     it('provides the correct default for the chosen dbms', function () {
-        const askStub = sinon.stub(inquirer, 'prompt').onCall(0).resolves({ dbms: cst.dbmsList.mysql.name });
-        askStub.onCall(1).resolves(null);
+        const inquirerPromptStub = sandbox.stub(inquirer, 'prompt').onCall(0).resolves({ dbms: cst.dbmsList.mysql.name });
+        inquirerPromptStub.onCall(1).resolves(null);
 
         const expectedPortInq = Object.assign({ default: 3306 }, inq.port);
 
         return prompt.askCredentials()
             .then((onFulfilled) => {
-                assert.deepEqual(askStub.getCall(1).args[0], [inq.host, expectedPortInq, inq.user, inq.password]);
+                assert.deepEqual(inquirerPromptStub.getCall(1).args[0], [inq.host, expectedPortInq, inq.user, inq.password]);
             });
     });
 
     it.skip('returns a fulfilled promise containing complete credentials', function () {
-        const askStub = sinon.stub(inquirer, 'prompt').withArgs(inq.dbms).resolves('mysql');
+        const askStub = sandbox.stub(inquirer, 'prompt').withArgs(inq.dbms).resolves('mysql');
 
         console.log(prompt.askCredentials())
             .then((onFulfilled) => {
