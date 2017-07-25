2c9fa1e7e5cd452927684a213881fa4006f42217
AdrienHorgnies <aHorgnies@altissia.com>
2017-07-25 17:12:05 +0200

    remove now useless tests (sigh) and put new simpler one (again... sigh)

--- test/test-prompt.js
+++ test/test-prompt.js
@@ -6,7 +6,7 @@ const prompt = require('../prompt');
 const cst = require('../constants');
 
 const sandbox = sinon.sandbox.create();
-const inq = cst.inquiries;
+const inquiries = cst.inquiries;
 
 describe('prompt', function () {
     describe('askCredentials', function () {
@@ -16,40 +16,20 @@ describe('prompt', function () {
             sandbox.restore();
         });
 
-        it('provides the correct default port for the chosen dbms', function () {
-            // This replaces the function inquirer.prompt with a stub. I do so it doesn't need to prompt a user.
-            const inquirerPromptStub = sandbox.stub(inquirer, 'prompt').onFirstCall().resolves({ dbms: 'mysql' });
-            inquirerPromptStub.onSecondCall().resolves(null); // I don't care what it returns
+        it('was called with expected arguments', function () {
+            // This replaces the function inquirer.prompt with a stub.
+            const stub = sandbox.stub(inquirer, 'prompt');
 
-            const expectedPortInq = Object.assign({ default: 3306 }, inq.port);
+            prompt.askCredentials();
 
-            return prompt.askCredentials()
-                .then((onFulfilled) => {
-                    assert.deepEqual(inquirerPromptStub.getCall(1).args[0], [inq.host, expectedPortInq, inq.user, inq.password]);
-                });
-        });
-
-        it('returns a fulfilled promise containing complete credentials', function () {
-            const inquirerPromptStub = sandbox.stub(inquirer, 'prompt').onFirstCall().resolves({ dbms: 'mysql' });
-            inquirerPromptStub.onSecondCall().resolves({
-                host: '192.168.32.2',
-                port: '10485',
-                user: 'adminIsKing',
-                password: 'VeryStrongAndSecretPassword'
-            });
-
-            return prompt.askCredentials()
-                .then((onFulfilled) => {
-                    assert.deepEqual(
-                        onFulfilled,
-                        {
-                            dbms: 'mysql',
-                            host: '192.168.32.2',
-                            port: '10485',
-                            user: 'adminIsKing',
-                            password: 'VeryStrongAndSecretPassword'
-                        });
-                });
+            assert.deepEqual(stub.getCall(0).args[0], [
+                inquiries.dbms,
+                inquiries.host,
+                inquiries.port,
+                inquiries.user,
+                inquiries.password,
+                inquiries.schema
+            ]);
         });
     });
 });
