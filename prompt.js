b288af66d9685af174014b1645d6c5eda2e80c08
AdrienHorgnies <aHorgnies@altissia.com>
2017-07-26 10:42:11 +0200

    add function to create validation method

--- prompt.js
+++ prompt.js
@@ -1,7 +1,23 @@
 const inquirer = require('inquirer');
 const cst = require('./constants');
+const joi = require('joi');
 
 const inquiries = cst.inquiries;
+
+/**
+ *
+ * @param rule created with the validation framework joi
+ * @returns function (input) that returns true if input passes validation of rule, the error otherwise
+ */
+const validation = rule =>
+    (input) => {
+        const error = joi.validate(input, rule).error;
+        if (error) {
+            return error;
+        }
+        return true;
+    };
+
 
 const askCredentials = () => inquirer.prompt([
     inquiries.dbms,
@@ -13,5 +29,6 @@ const askCredentials = () => inquirer.prompt([
 ]);
 
 module.exports = {
+    validation,
     askCredentials
 };
