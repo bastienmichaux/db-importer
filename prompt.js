528e89a7753216cb0c13030f20003b74b1da3dd7
AdrienHorgnies <aHorgnies@altissia.com>
2017-07-26 12:03:17 +0200

    move the validation method to its own file

--- prompt.js
+++ prompt.js
@@ -1,22 +1,7 @@
 const inquirer = require('inquirer');
 const cst = require('./constants');
-const joi = require('joi');
 
 const inquiries = cst.inquiries;
-
-/**
- *
- * @param rule created with the validation framework joi
- * @returns function (input) that returns true if input passes validation of rule, the error otherwise
- */
-const validation = rule =>
-    (input) => {
-        const error = joi.validate(input, rule).error;
-        if (error) {
-            return error;
-        }
-        return true;
-    };
 
 
 const askCredentials = () => inquirer.prompt([
@@ -29,6 +14,5 @@ const askCredentials = () => inquirer.prompt([
 ]);
 
 module.exports = {
-    validation,
     askCredentials
 };
--- test/test-prompt.js
+++ test/test-prompt.js
@@ -33,40 +33,4 @@ describe('prompt', function () {
             ]);
         });
     });
-
-    describe('validation', function () {
-        const rule = joi
-            .string()
-            .allow('localhost')
-            .ip({
-                version: ['ipv4', 'ipv6'],
-                cidr: 'forbidden'
-            });
-        const validationMethod = prompt.validation(rule);
-
-        it('returns true when joi would return a null error', function () {
-            const validInput = 'localhost';
-
-            assert(validationMethod(validInput));
-        });
-
-        it('returns the error joi would put in his return object', function () {
-            const emptyInput = '';
-            const invalidInput = '256.0.0.1';
-
-            assert.deepEqual(validationMethod(emptyInput), joi.validate(emptyInput, rule).error);
-            assert.deepEqual(validationMethod(invalidInput), joi.validate(invalidInput, rule).error);
-        });
-
-        it('calls joi.validate with the provided joi schema and input', function () {
-            const validateSpy = sandbox.spy(joi, 'validate');
-
-            const anyInput = {};
-
-            validationMethod(anyInput);
-
-            assert.equal(validateSpy.getCall(0).args[0], anyInput);
-            assert.equal(validateSpy.getCall(0).args[1], rule);
-        });
-    });
 });
