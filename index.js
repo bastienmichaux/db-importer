63f72785d234f598ba7d2c1884f1b275aa112d3d
Bastien Michaux <bastienmichaux@gmail.com>
2017-08-21 16:17:34 +0200

    add doc to index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// dbi library
+/**
+ * @file Entry point of the program
+ */
+
 const cst = require('./constants');
 const prompt = require('./prompt');
 const log = require('./lib/log');
@@ -6,13 +9,25 @@ const db = require('./lib/db-commons');
 
 const msg = cst.messages;
 
+// greet the user
 log.emphasize(msg.greeting);
 
+/**
+ * Ask the user for credentials,
+ * then what database should be imported
+ */
 prompt.init()
+    // get connection credentials from the user & validate them
+    // returns session credentials
     .then(prompt.askCredentials)
+    // attempt connection to the database using the session credentials
     .then(db.connect)
+    // display retrieved tables
     .then(db.entityCandidates)
+    // ask the user what tables should be converted to JSON entities
     .then(prompt.selectEntities)
+    // @todo convert tables to JSON entities
+    // close the connection, in case of error try again
     .then(db.close, (error) => {
         console.log(error);
         return prompt.askCredentials().then(db.connect).then(db.close);
