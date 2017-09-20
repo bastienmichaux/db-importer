da861ded202803142a6f94e1102f856f249b98a8
AdrienHorgnies <aHorgnies@altissia.com>
2017-09-20 10:46:40 +0200

    align tests describes to other tests and indent

--- test/test-mysql-queries.js
+++ test/test-mysql-queries.js
@@ -147,63 +147,76 @@ const dummySchema = mysql.escape('dummy_schema');
 const dummyFilter = mysql.escape(['filtered_table_1', 'filtered_table_2', 'last_filtered_table']);
 const dummyTables = mysql.escape(['table_1', 'table_2', 'last_table']);
 
-describe('./lib/mysql/queries.js', function () {
-    describe('liquibase query', function () {
+describe('lib/mysql/queries', function () {
+    describe('liquibase', function () {
         it('returns the expected query', function () {
             assert.strictEqual(queries.liquibase(dummySchema), liquibaseQuery);
         });
     });
-    describe('jhipster query', function () {
+
+    describe('jhipster', function () {
         it('returns the expected query', function () {
             assert.strictEqual(queries.jhipster(dummySchema), jhipsterQuery);
         });
     });
-    describe('manyToMany query', function () {
+
+    describe('manyToMany', function () {
         it('returns the expected query', function () {
             assert.strictEqual(queries.manyToMany(dummySchema), manyToManyQuery);
         });
     });
-    describe('manyToOne query', function () {
+
+    describe('manyToOne', function () {
         it('returns the expected query', function () {
             assert.strictEqual(queries.manyToOne(dummySchema), manyToOneQuery);
         });
     });
-    describe('oneToOne query', function () {
+
+    describe('oneToOne', function () {
         it('returns the expected query', function () {
             assert.strictEqual(queries.oneToOne(dummySchema), oneToOneQuery);
         });
     });
-    describe('columns query', function () {
+
+    describe('columns', function () {
         it('returns query that only retrieves the given tables', function () {
             assert.strictEqual(queries.columns(dummySchema, dummyTables), columnsQuery);
         });
+
         it('returns query that doesn\'t exclude more tables', function () {
             assert.strictEqual(queries.columns(dummySchema, ''), columnsQueryWhenNoTables);
         });
+
         it('throws with parameters that aren\'t strings', function () {
             assert.throws(() => queries.columns(['dummy_schema'], null), TypeError);
             assert.throws(() => queries.columns(dummySchema, null), TypeError);
         });
     });
-    describe('twoTypeJunction query', function () {
+
+    describe('twoTypeJunction', function () {
         it('returns query that excludes tables according to their TABLE_NAME', function () {
             assert.strictEqual(queries.twoTypeJunction(dummySchema, dummyFilter), twoTypeJunctionQueryWithFilter);
         });
+
         it('returns query that doesn\'t that excludes tables according to their TABLE_NAME', function () {
             assert.strictEqual(queries.twoTypeJunction(dummySchema, ''), twoTypeJunctionQueryWithoutFilter);
         });
+
         it('throws with parameters that aren\'t strings', function () {
             assert.throws(() => queries.twoTypeJunction(['dummy_schema'], null), TypeError); // 1st param throws error
             assert.throws(() => queries.twoTypeJunction(dummySchema, null), TypeError); // 2nd param throws error
         });
     });
-    describe('tables query', function () {
+
+    describe('tables', function () {
         it('returns query that excludes tables according to their TABLE_NAME', function () {
             assert.strictEqual(queries.tables(dummySchema, dummyFilter), tablesQueryWithFilter);
         });
+
         it('returns query that doesn\'t that excludes tables according to their TABLE_NAME', function () {
             assert.strictEqual(queries.tables(dummySchema, ''), tablesQueryWithoutFilter);
         });
+
         it('throws with parameters that aren\'t strings', function () {
             assert.throws(() => queries.tables(['dummy_schema'], null), TypeError);
             assert.throws(() => queries.tables(dummySchema, null), TypeError);
