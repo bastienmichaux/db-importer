5b9593f61b557939d370c2c81b07811568832459
AdrienHorgnies <aHorgnies@altissia.com>
2017-10-03 09:53:50 +0200

    clean travis directory

--- travis/scripts/query-mysql.sh
+++ travis/scripts/query-mysql.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -ev
 
+mkdir dev-life-dir
+cd dev-life-dir
+
 mysql < "$MYSQL"/dev_life.sql
 
-cp -t . "$MYSQL"/.db-config.json
+cp "$MYSQL"/dev-life-config.json .db-config.json
 
-node index
+node ../index
 
 diff db-export.json "$MYSQL"/dev-life-export.json
+
+rm .db-config.json
+rm db-export.json
+
+cd ..
+rmdir dev-life-dir
